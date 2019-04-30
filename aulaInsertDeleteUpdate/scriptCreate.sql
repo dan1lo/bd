e74f62b4ffa724847ec046f5bdb103021a322426
danilo <danilomonteiroo@gmail.com>
2019-04-30 10:45:20 -0300

    script para criat tabela

--- aulaInsertDeleteUpdate/scriptCreate.sql
+++ aulaInsertDeleteUpdate/scriptCreate.sql
@@ -1,4 +1,12 @@
+
+
 CREATE DATABASE pague_menos; -- para criar o bd, se for usar o pgadmin não precisa desse comando
+
+-- para executar o script, você deve abrir o pg admin, criar um servidor(em geral tah criado)
+-- depois criar uma tabela ( pague_menos) por exemplo
+-- botao direito no nome da tabela e query tools
+-- copia e cola e aperta no trovão (executar)
+
 CREATE TABLE farmacias (
 cnpj INT, -- cpf e cnpj eh inteiro e nao serial porque é um num fixo que eh fornecido pelo cliente
 dono TEXT,
