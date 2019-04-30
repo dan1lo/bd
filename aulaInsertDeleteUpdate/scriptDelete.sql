6171b74cbce8938ef8f5fc0a44da7c093416b762
danilo <danilomonteiroo@gmail.com>
2019-04-30 11:37:15 -0300

    scriptupdate V4

---
+++ aulaInsertDeleteUpdate/scriptDelete.sql
@@ -0,0 +1,12 @@
+--execute esse script por ultimo
+-- ele vai apagar as tuplas do banco
+
+
+--SINTAXE
+--
+--DELETE FROM <nome da tabela> 
+-- 	WHERE  <condicao>; 
+
+
+--aqui não tem grande mágica...
+DELETE FROM farmacia WHERE id = 8;
