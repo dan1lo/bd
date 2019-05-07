640c8dc0d5ce5f95b6034dd5dcbd1c499a6cde43
danilo <danilomonteiroo@gmail.com>
2019-05-07 12:30:49 -0300

    aula select - arrumar VARIOS BUGS

--- aulaSelect/comandosAulaSelect.sql
+++ aulaSelect/comandosAulaSelect.sql
@@ -18,17 +18,16 @@
 -- busca tudo na tabela
 SELECT * FROM enfeites;
 -- busca somente as colunas nome e cor
-
-SELECT nome, cor FROM enfeites;
+SELECT nome, cor FROM enfeites;
 
 -------
-select * from enfeites
+
 -- busca todas as colunas na tabela enfeite onde os enfeites tenham o tamanho 5
-SELECT * FROM enfeites WHERE tamanho = 5;
+SELECT * FROM enfeites WHERE tamanho = 5;
 
 --Operador de igual (=)
 SELECT cor FROM enfeites WHERE tamanho = 2;
-SELECT nome FROM enfeites WHERE nome = 'arvore': -- podemos usar mas nao é recomendavel usar o = com string
+SELECT nome FROM enfeites WHERE nome = 'arvore'; -- podemos usar mas nao é recomendavel usar o = com string
 --Operador de diferente (<>)
 SELECT tamanho FROM enfeites WHERE valor <> 3;
 --Operador de Maior (>) ou menor (<)
@@ -37,9 +36,10 @@ SELECT * FROM enfeites WHERE valor < 3;
 SELECT id FROM enfeites WHERE valor >= 2;
 --Operador Like (%)
 SELECT * FROM enfeites WHERE cor LIKE 'ver%';
-SELECT * FROM enfeites WHERE cor LIKE '%e ';
+SELECT * FROM enfeites WHERE cor LIKE '%e';
 SELECT * FROM enfeites WHERE cor LIKE '%o%';
 SELECT * FROM enfeites WHERE cor ILIKE '%VER';
+SELECT * FROM enfeites WHERE cor ILIKE '%VER%';
 
 -- orderby ordenar os resultados
 --ASC - menor para o maior (1,2,3,4,20)
@@ -47,7 +47,8 @@ SELECT * FROM enfeites WHERE cor ILIKE '%VER';
 
 SELECT nome FROM enfeites ORDER BY tamanho DESC;
 
-SELECT * FROM enfeites WHERE cor like ’vermelho’ ORDER BY id DESC; -- estou ordenando os enfeits vermelhos pelo id do maior para o menor
+
+SELECT * FROM enfeites WHERE cor like 'vermelho' ORDER BY  id DESC; -- estou ordenando os enfeits vermelhos pelo id do maior para o menor
 
 --between
 -- comando between é entre dois valores
@@ -66,13 +67,13 @@ SELECT nome FROM enfeites ORDER BY  valor DESC LIMIT 2;
 
 -- posso usar operacçoes
 
-SELECT nome, salario*2 FROM farmaceuticos
-SELECT nome, salario+2 FROM farmaceuticos
-SELECT nome, salario-2 FROM farmaceuticos
-SELECT nome, salario/2 FROM farmaceuticos
+SELECT nome, salario*2 FROM farmaceuticos;
+SELECT nome, salario+2 FROM farmaceuticos;
+SELECT nome, salario-2 FROM farmaceuticos;
+SELECT nome, salario/2 FROM farmaceuticos;
 
 -- aqui eu dou um apelido para o calculo, e a coluna vai se chamar metade 
-SELECT nome, salario/2 as metade FROM farmaceuticos
+SELECT nome, salario/2 as metade FROM farmaceuticos;
 
 
 
@@ -83,14 +84,14 @@ SELECT avg(salario) as media_salario FROM farmaceuticos;
 
 
 -- contar quantos tem count (campo)
-SELECT       count(id) FROM farmaceuticos;
+SELECT count(CPF) FROM farmaceuticos;
 
-SELECT       count(id) FROM farmaceuticos WHERE salario>600;
+SELECT count(CPF) FROM farmaceuticos WHERE salario>400;
 
 -- distinct = diferntes
 
 -- quantos nomes diferentes...
-SELECT      distinct(nome) FROM farmaceuticos;
+SELECT distinct(nome) FROM farmaceuticos;
 
 
 -- MAX = VALOR MAXIMO
@@ -102,12 +103,12 @@ SELECT      MAX(SALARIO) FROM farmaceuticos;
 
 -- MIN = MENOR
 
-SELECT      Min(SALARIO) FROM farmaceuticos;
+SELECT Min(SALARIO) FROM farmaceuticos;
 
 
 -- SUM = SOMA DE TODOS
 
-SELECT      sum(SALARIO) FROM farmaceuticos;
+SELECT sum(SALARIO) FROM farmaceuticos;
 
 
 
